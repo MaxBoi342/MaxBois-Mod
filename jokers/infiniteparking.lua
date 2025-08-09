8946e50273bea3a269197e46debc9cc127cb8fb5
Maxim Savitski <m.a.savitski@student.tudelft.nl>
2025-08-09 03:49:33 +0200

    added infinite parking joker

---
+++ jokers/infiniteparking.lua
@@ -0,0 +1,25 @@
+SMODS.Joker { --Infinite Parking
+    key = "infiniteparking",
+    config = {
+        extra = {
+        }
+    },
+    loc_txt = {
+        ['name'] = 'Infinite Parking',
+        ['text'] = {
+            [1] = 'PLACEHOLDER',
+        }
+    },
+    pos = {
+        x = 2,
+        y = 1
+    },
+    cost = 4,
+    rarity = 1,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = true,
+    atlas = 'CustomJokers'
+}
